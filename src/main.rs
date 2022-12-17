efcfd7562db1ed850a9d8a69a2607c95f2ed5b70
Jacob Morehouse <jacob.morehouse@gmail.com>
2022-12-16 20:35:36 -0800

    Added menu for clear and quit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io};
 use rand::Rng;
 
 #[allow(non_snake_case)]
@@ -7,17 +7,12 @@ fn main() {
     let lowest_num: i32 = 1;
     let highest_num: i32 = 10;
 
-    //TODO:  make menu to exit loop
-
-    //TODO: clear the console each time so that it looks cleaner.
-
-
+    clear_console();
     println!("----- Guessing Game ----- ");
 
     loop {
         let mut some_input:String = String::new();
-        let mut random_number: i32 = generate_rand(lowest_num, highest_num);
-
+        let random_number: i32 = generate_rand(lowest_num, highest_num);
 
         println!("Please guess a number between {} and {}: ", lowest_num, highest_num);
 
@@ -30,9 +25,17 @@ fn main() {
             Ok(_i) => process_input(
                 some_input.trim().parse().expect("numeric"), random_number
             ),
-            Err(..) => println!("This was not an integer: {}", trimmed),
-        };
-
+            Err(..) => {
+                if trimmed.to_lowercase() == "q" {
+                    clear_console();
+                    std::process::exit(0);
+                } else if trimmed.to_lowercase() == "c" {
+                    clear_console();
+                } else {
+                    println!("Not acceptable input. Options: \n {} - {} numeric guess \n q: Quit. \n c: Clear screen. ", lowest_num, highest_num)
+                }
+            }
+        }
     }
 
 }
@@ -44,11 +47,15 @@ fn generate_rand(in_low: i32, in_high: i32) -> i32 {
 }
 
 fn process_input(user_input: i32, random_input: i32) {
-
     if user_input == random_input {
         println!("Correct! High five! {}", random_input)
     } else {
         println!("Too bad. {}", random_input)
     }
+}
+
+fn clear_console() {
+    //Just wanted to give it a better name. 
+    print!("\x1B[2J\x1B[1;1H"); //Clears the console and moves the cursor to the top.
 }
 
